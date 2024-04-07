e5e08ec8ac97bdbd0f908f49aa1492210de2a2de
Arpit Taksande <arpittaksande@gmail.com>
2024-04-08 00:53:13 +0530

    Add New Project

---
+++ sample.py
@@ -0,0 +1,19 @@
+sample=f'''\tCompany Name ,XYZ\n\tAddress:Xyz,floor4
+-----------------------------------------
+ Employee ID\t\t:  1024
+ salary of \t\t:  Dec_2020
+ Generated On\t\t:  20-12-2020
+------------------------------------------
+ Total Days\t\t:  31
+ Total Present\t\t:  20
+ Total Absent\t\t:  10
+ Convence\t\t:  Rs.1000
+ Medical\t\t:  Rs.2000
+ PF\t\t:  Rs.1500
+ Gross Payment\t\t:  Rs.3562562
+ Net Salary\t\t:  Rs.25455222
+------------------------------------------
+This is computer generated slip, not 
+requrired any signature
+'''
+
